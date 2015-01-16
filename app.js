760ee3272043e8c5f30d4fc76a622b136b43f65d
Peter Hastie <phastie@bleacherreport.com>
2015-01-15 23:12:51 -0800

    Sass to build stylesheets.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var path = require('path');
 
 var express = require('express');
+var sass = require('node-sass-middleware');
 
 var middleware = require('./app/middleware/middleware');
 
@@ -16,6 +17,12 @@ app.set('view engine', 'jade');
 // Configure middleware
 app.use(middleware.favicon(app));
 app.use(middleware.logger());
+app.use(sass({
+	src: path.join(__dirname, 'assets', 'scss'),
+	dest: path.join(__dirname, 'public', 'stylesheets'),
+	outputStyle: 'compressed',
+	prefix: '/stylesheets'
+}));
 app.use(express.static(path.join(__dirname, 'bower_components')));
 app.use(express.static(path.join(__dirname, 'public')));
 
