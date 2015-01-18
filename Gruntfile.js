70b20e2b744ca265025458f7b8b8d0b52585c8f3
Peter Hastie <phastie@bleacherreport.com>
2015-01-17 22:16:53 -0800

    Moved javascripts to include files for processing with Grunt.

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		concat: {
+			options: {
+				separator: ';'
+			}
+		},
+		jadeUsemin: {
+			prod: {
+				options: {
+					tasks: {
+						js: ['concat', 'uglify']
+					}
+				},
+				files: [{
+					src: './assets/includes/main.jade',
+					dest: './app/views/includes/main.jade'
+				}]
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-jade-usemin');
+
+	grunt.registerTask('default', ['jadeUsemin:prod']);
+};
