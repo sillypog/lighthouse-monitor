1a1a010661bffa5669d236fd357a9ec5262ff7af
Peter Hastie <phastie@bleacherreport.com>
2015-02-09 16:29:59 -0800

    Resolved gitignore conflict.

---
+++ test/mocha-test.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+
+describe('mocha', function(){
+	describe('assert', function(){
+		it('should pass valid conditions', function(){
+			assert.equal(true, true);
+		});
+
+		/*it('should fail invalid conditions', function(){
+			assert.equal(true, false);
+		});*/
+	});
+});
